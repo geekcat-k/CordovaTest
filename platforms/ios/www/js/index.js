4d07ac8e6407f8ee57600c0391c55dba2381272c
森川 隆之 <morio@MacBookAir.local>
2017-12-03 18:22:21 +0900

    sample

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,46 +1,34 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        this.bindEvents();
     },
-
+    // Bind Event Listeners
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
     // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+        //  （1）画面表示の切り替え
+        $('#device-not-ready').hide();
+        $('#device-ready').show();
+
+        // （2） ボタンによるカメラの起動
+        $('#camera-call').click(function(){
+            // （3） カメラの起動と、画像の取り込み手法、結果Callbackの設定
+            navigator.camera.getPicture(app.onSuccess, app.onFail, { quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL
+            });
+        });
     },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    // （4） カメラで撮った写真データの表示
+    onSuccess : function(imageData){
+        var image = $('#image');
+        image.attr('src',"data:image/jpeg;base64," + imageData);
+    },
+    // （5） カメラの起動失敗などの場合
+    onFail : function(message) {
+        alert('Failed because: ' + message);
     }
 };
-
 app.initialize();
